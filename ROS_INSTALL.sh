b8fe382d4c6cc150416b9f9fb058a627cc4156a2
Loren <Loren.sarthur@gmail.com>
2016-02-29 12:27:01 -0800

    Script to install ROS on the bone

---
+++ ROS_INSTALL.sh
@@ -0,0 +1,12 @@
+update-locale LANG=C LANGUAGE=C LC_ALL=C LC_MESSAGES=POSIX
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
+apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
+apt-get update
+apt-get install ros-indigo-ros-base
+apt-get install python-rosdep
+rosdep init
+rosdep update
+echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt-get install python-rosinstall
