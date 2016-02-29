b9020091238a5730a0186dc97ef53514bd8f9802
Loren <Loren.sarthur@gmail.com>
2016-02-29 13:31:16 -0800

    Added ADAFruit spi dts

--- ROS_INSTALL.sh
+++ ROS_INSTALL.sh
@@ -1,12 +1,12 @@
-update-locale LANG=C LANGUAGE=C LC_ALL=C LC_MESSAGES=POSIX
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
-apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
-apt-get update
-apt-get install ros-indigo-ros-base
-apt-get install python-rosdep
-rosdep init
+sudo update-locale LANG=C LANGUAGE=C LC_ALL=C LC_MESSAGES=POSIX
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
+sudo apt-get update
+sudo apt-get install ros-indigo-ros-base
+sudo apt-get install python-rosdep
+sudo rosdep init
 rosdep update
 echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
-apt-get install python-rosinstall
+sudo apt-get install python-rosinstall
